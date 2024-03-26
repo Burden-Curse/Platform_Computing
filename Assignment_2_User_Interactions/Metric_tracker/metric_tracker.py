68f20186d5af58904b13724f6c2bced2cc1bf240
Ken <58831340+Burden-Curse@users.noreply.github.com>
2024-03-25 22:55:07 -0700

    Publishing to Github
    Fix metric tracker before pushing to main

--- Assignment_2_User_Interactions/Metric_tracker/metric_tracker.py
+++ Assignment_2_User_Interactions/Metric_tracker/metric_tracker.py
@@ -3,52 +3,91 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Initialize browser
-driver = webdriver.Firefox() #I don't have Chrome, change this to preferred browser
+import mysql.connector
 
-#Trying to get my href button
-#button = driver.find_element(By.CSS_SELECTOR, value="a")
+def WritetoCSV(filename : str, metrics : dict):
+    with open(file=filename, mode='w', newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=metrics[0].keys())
+        writer.writeheader()
 
-#Trying to get number of paragraphs in my webpage
-Number_of_p = driver.find_elements(By.CSS_SELECTOR, "#everything .p")
+        for i in metrics:
+            writer.writerow(i)
 
-# Navigate to your website 
-Title = driver.get("http://localhost:3000/")
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="", #Blank on purpose
+    database="cse4500"
+)
 
-driver.implicitly_wait(0.5) #probably helps for a buffer
+mycursor = mydb.cursor()
 
-metrics = []
-# Track presence time 
-start_time = time.time()
-presence_time = start_time
-while True == True:#presence_time < 50: # seconds
-    current_time = time.time()
-    presence_time = current_time - start_time
-    
-    # Track scrolling
-    #scroll_height = driver.execute_script("return document.body.scrollHeight")
-    Title_name = driver.execute_script('return document.title;')
-    current_scroll = driver.execute_script("return window.pageYOffset;")
+sqlFormula = "INSERT INTO measurements1 (`TimeStamp`, `Presence Time`, `Scrolling`, `Title Name`) VALUES (%s, %s, %s, %s)"
 
-    #########
-    #Optional
-    #########
-    # for button in buttons:
-    #     button.click()
-    #     num_clicks += 1
-    Number = 0
-    for i in Number_of_p:
-        Number += i
-    #Working, don't touch
-    #Might make it into a Dict later on
-    Name = ["Current Scroll Value", "Time Spent in Website", "Title Page"]
-    metrics = [[current_scroll, presence_time, Title_name]]
-    with open('Measurements.csv', mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(Name)
-        csv_writer.writerows(metrics)
+def main():
+    # Initialize browser
+    driver = webdriver.Firefox() #I don't have Chrome, change this to preferred browser
 
-driver.quit()
+    #Trying to get my href button
+    #button = driver.find_element(By.CSS_SELECTOR, value="a")
+
+    #Trying to get number of paragraphs in my webpage
+    Number_of_p = driver.find_elements(By.CSS_SELECTOR, "#everything .p")
+
+    # Navigate to your website 
+    Title = driver.get("http://localhost:3000/")
+
+    driver.implicitly_wait(0.5) #probably helps for a buffer
+
+    metrics = []
+    # Track presence time
+    count = 0
+    SAMPLE_SIZE = 10
+    start_time = time.time()
+    presence_time = start_time
+    TimeStamp = time.strftime("%H:%M:%S", time.localtime())
+    while count < SAMPLE_SIZE:#presence_time < 50: # seconds
+        current_time = time.time()
+        presence_time = current_time - start_time
+        
+        # Track scrolling
+        #scroll_height = driver.execute_script("return document.body.scrollHeight")
+        Title_name = driver.execute_script('return document.title;')
+        current_scroll = driver.execute_script("return window.pageYOffset;")
+        TimeStamp = time.strftime("%H:%M:%S", time.localtime())
+        #########
+        #Optional
+        #########
+        # for button in buttons:
+        #     button.click()
+        #     num_clicks += 1
+        Number = 0
+        for i in Number_of_p:
+            Number += i
+        #Working, don't touch
+        #Might make it into a Dict later on
+        
+        mycursor.execute(sqlFormula, (TimeStamp, presence_time, current_scroll, Title_name))
+
+        metrics.append({"TimeStamp (HH:MM:SS)": TimeStamp,
+                        "Presense Time (Seconds)" : presence_time,
+                        "Scrolling (current Pixel)" : current_scroll,
+                        "Title Name" : Title_name})
+        
+        time.sleep(2)
+        count += 1
+        
+        
+    driver.quit()
+    WritetoCSV("Measurements.csv", metrics)
+    mydb.commit()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 ###############################################
 #Leaving in all my fail attempts for referrence
---
+++ Assignment_3_Database/Metric_tracker/metric_tracker.py
@@ -0,0 +1,71 @@
+import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def WritetoCSV(filename : str, metrics : dict):
+    with open(file=filename, mode='w', newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=metrics[0].keys())
+        writer.writeheader()
+
+        for i in metrics:
+            writer.writerow(i)
+
+def main():
+    # Initialize browser
+    driver = webdriver.Firefox() #I don't have Chrome, change this to preferred browser
+
+    #Trying to get my href button
+    #button = driver.find_element(By.CSS_SELECTOR, value="a")
+
+    #Trying to get number of paragraphs in my webpage
+    Number_of_p = driver.find_elements(By.CSS_SELECTOR, "#everything .p")
+
+    # Navigate to your website 
+    Title = driver.get("http://localhost:3000/")
+
+    driver.implicitly_wait(0.5) #probably helps for a buffer
+
+    metrics = []
+    # Track presence time
+    count = 0
+    SAMPLE_SIZE = 10
+    start_time = time.time()
+    presence_time = start_time
+    while count < SAMPLE_SIZE:#presence_time < 50: # seconds
+        current_time = time.time()
+        presence_time = current_time - start_time
+        
+        # Track scrolling
+        #scroll_height = driver.execute_script("return document.body.scrollHeight")
+        Title_name = driver.execute_script('return document.title;')
+        current_scroll = driver.execute_script("return window.pageYOffset;")
+
+        #########
+        #Optional
+        #########
+        # for button in buttons:
+        #     button.click()
+        #     num_clicks += 1
+        Number = 0
+        for i in Number_of_p:
+            Number += i
+        #Working, don't touch
+        #Might make it into a Dict later on
+        metrics.append({"TimeStamp (HH:MM:SS)": time.strftime("%H:%M:%S", time.localtime()),
+                        "Presense Time (Seconds)" : presence_time,
+                        "Scrolling (current Pixel)" : current_scroll,
+                        "Title Name" : Title_name})
+        
+        time.sleep(2)
+        count += 1
+        
+    driver.quit()
+    WritetoCSV("Measurements.csv", metrics)
+
+    
+
+if __name__ == "__main__":
+    main()
+
